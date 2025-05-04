69287854e00b1dcfd8ecd88dbbcbc1190177934f
Karim Hamed <karimhamed947@gmail.com>
2025-05-04 23:04:42 +0300

    Initial Commit

---
+++ SwiftUII/SwiftUIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIApp.swift
+//  SwiftUII
+//
+//  Created by Karim Hamed  on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
